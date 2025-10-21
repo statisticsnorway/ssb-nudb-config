d802a5d393fe290846b7c7abf1d46135a755fb2b
Carl F. Corneil <aecorn@gmail.com>
2025-10-21 17:07:52 +0200

    extra testing for sonarcloud

--- tests/test_dotmap.py
+++ tests/test_dotmap.py
@@ -1,5 +1,5 @@
 from nudb_config.pydantic.dotmap import DotMap
-
+import pytest
 
 def test_dotmap_get_basic() -> None:
     """Test the get convenience method on the Dotmap class."""
@@ -13,3 +13,12 @@ def test_dotmap_get_basic() -> None:
 
     # missing with default -> default
     assert dm.get("missing", 42) == 42
+
+    # Testing __contains__()
+    assert "a" in dm
+    
+    # Should raise an error if the element is not there
+    with pytest.raises(KeyError):
+        dm["c"]
+
+
