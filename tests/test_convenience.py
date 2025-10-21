8b1a4f3d604b3b7a25735cecea81dae6150826ff
Carl F. Corneil <aecorn@gmail.com>
2025-10-21 16:50:37 +0200

    adding test for convenience methods

---
+++ tests/test_convenience.py
@@ -0,0 +1,8 @@
+from nudb_config import settings
+
+def test_keys_on_single_variable():
+    var = settings.variables["snr"]
+    assert len(var.keys())
+    assert len(var.values())
+    assert len(var.items())
+    assert var.get("dtype")
