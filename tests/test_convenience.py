c963fee5d28e71d6a35427ee253f4c2018344c38
Carl F. Corneil <aecorn@gmail.com>
2025-10-21 16:57:03 +0200

    pre-commit and missing test return typing

--- tests/test_convenience.py
+++ tests/test_convenience.py
@@ -1,6 +1,7 @@
 from nudb_config import settings
 
-def test_keys_on_single_variable():
+
+def test_keys_on_single_variable() -> None:
     var = settings.variables["snr"]
     assert len(var.keys())
     assert len(var.values())
