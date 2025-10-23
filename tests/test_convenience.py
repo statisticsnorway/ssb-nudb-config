f5715e85b153733a104fafd8ce98e1f5d2d10234
Carl F. Corneil <aecorn@gmail.com>
2025-10-23 08:22:28 +0200

    Trying to restore functionality to getattr() on Dotmap class

--- tests/test_convenience.py
+++ tests/test_convenience.py
@@ -9,6 +9,12 @@ def test_keys_on_single_variable() -> None:
     assert var.get("dtype")
 
 
+def test_fetch_variable_different() -> None:
+    assert settings.variables.snr
+    assert settings.variables["snr"]
+    assert settings.variables.get("snr")
+
+
 def test_iteration() -> None:
     for elem in settings.variables:
         assert elem
