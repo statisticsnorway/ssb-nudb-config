0dcc7a41c8d6001b6ed499d15cf3f88ab78da789
Carl F. Corneil <aecorn@gmail.com>
2025-11-03 14:12:26 +0100

    complaints from tools

--- tests/test_variables_content.py
+++ tests/test_variables_content.py
@@ -1,6 +1,7 @@
 from nudb_config import settings
 
-def test_variables_sort_unit():
+
+def test_variables_sort_unit() -> None:
     variables_sort_unit = settings.variables.variables_sort_unit
     assert len(variables_sort_unit)
     assert all([isinstance(x, str) for x in variables_sort_unit])
