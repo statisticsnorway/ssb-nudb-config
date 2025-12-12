f76b045f5fd3e2dbeea118b5ab7fae9233c45086
kss2k <slupphaugkjell@gmail.com>
2025-12-12 10:29:37 +0100

    Add __version__ attribute

--- src/nudb_config/__init__.py
+++ src/nudb_config/__init__.py
@@ -3,3 +3,10 @@
 from .config import settings
 
 __all__ = ["settings"]
+
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except Exception as err:
+    __version__ = "0.0.0"
+    logger.warning(f"Unable to determine package version!\nMessage: {err}")
